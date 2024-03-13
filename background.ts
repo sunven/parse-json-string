e72941977effb74c280cbd8c6c0f99f693b75c69
wenquansun <604164227@qq.com>
2024-03-13 21:07:14 +0800

    feat: add bg

---
+++ background.ts
@@ -0,0 +1,6 @@
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
+  const msg = `Navigation to ${e.request.url} redirected on tab ${e.request.tabId}.`
+  console.log(msg)
+})
+
+console.log("Service worker started.")
